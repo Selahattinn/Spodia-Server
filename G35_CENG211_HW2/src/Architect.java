b5e6b2de1ca3360b6540979b44f211fb7916d3ba
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2019-11-04 21:09:46 +0300

    Gereksizler Silindi
    Sildim be

--- G35_CENG211_HW2/src/Architect.java
+++
@@ -1,9 +0,0 @@
-
-public class Architect extends Person {
-	
-	public Architect(String name,String birtdate,String deathdate,String country) {
-		super(name, birtdate, deathdate, country);	
-	}
-	
-
-}
