7acb2424751184da35685a3c730b9f1aa68abd2a
selahattin ceylan <selahattin.ceylan@hotmail.com>
2019-11-06 19:51:12 +0300

    server oluşturuldu
    kısmı hatalar var static kontrol yapılıyor şu anlık ve header yerine body ile gönderiliyor data .Denemelik üzerinde oynanıcak

---
+++ Server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	Addr     = flag.String("addr", ":8000", "TCP address to listen to")
+	Compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+)
+
+func main() {
+	flag.Parse()
+
+	Handler := RequestHandler
+	if Compress {
+		Handler = fasthttp.CompressHandler(Handler)
+	}
+
+	if ListenErr := fasthttp.ListenAndServeTLS(Addr, "MyCertificate.crt", "MyKey.key", Handler); ListenErr != nil {
+		log.Fatalf("Error in ListenAndServeTLS: %s", ListenErr)
+	}
+
+}
+
+//request handing
+func RequestHandler(ctx *fasthttp.RequestCtx) {
+
+	if string(ctx.Path()) == "/login" {
+		if string(ctx.Request.Body()) == "selo" {
+			ctx.SetBodyString("okey")
+		}
+
+	}
+
+	ctx.SetContentType("text/plain; charset=utf8")
+
+	// Set arbitrary headers
+	ctx.Response.Header.Set("X-My-Header", "my-header-value")
+
+}
+
+func IsError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
+}
